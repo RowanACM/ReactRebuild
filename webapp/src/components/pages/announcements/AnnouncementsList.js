ace41a8b00d3945c072c5b05c472e9151a649173
caow2 <caow2@students.rowna.edu>
2018-10-12 15:44:27 -0400

    Updated AnnouncementsList without any styling

--- webapp/src/components/pages/announcements/AnnouncementsList.js
+++ webapp/src/components/pages/announcements/AnnouncementsList.js
@@ -2,28 +2,30 @@ import React from 'react'
 import AnnouncementCard from './AnnouncementCard'
 
 export default class AnnouncementsList extends React.Component {
-    constructor() {
+    constructor(props) {
         super();
+        this.state={
+            announcementList:[]
+        }
     }
 
     componentWillMount() {
 
     }
     componentDidMount() {
-
-
+        fetch("/GetAnnouncements")
+        .then((res)=>{return res.json()})
+        .then(json=>this.setState({announcementList:json}));
     }
 
-
-
-
-
     render () {
-
         return (
-            <div>
-
-                
+            <div class="container">
+                {this.state.announcementList.map((announcement, i) =>
+                    {
+                        return <div><AnnouncementCard title={announcement.title} text={announcement.text} postedDate={announcement.postedDate}><div>
+                    })
+                }
             </div>
         )
     }
