8431d0596939d5ddde2caae21c4f1547728a0d26
DanAndresSanchez <sanchezd6@students.rowan.edu>
2018-10-19 15:00:44 -0400

    Added Navbar

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './components/Layout';
+import Jumbotron from "./components/common/Jumbotron"
 import CommitteeMemberCard from'./components/pages/committees/committee_members/CommitteeMemberCard';
 import AnnouncementForm from './components/pages/admin/AnnouncementForm';
 import CommitteeMemberCardList from './components/pages/committees/committee_members/CommitteeMemberCardList';
@@ -9,7 +10,10 @@ import CommitteeMemberCardList from './components/pages/committees/committee_mem
 
 
 ReactDOM.render(
-    /*<CommitteeMemberCardList committee={"Full Stack"}/>,*/
+<<<<<<< Updated upstream
+    
+=======
+>>>>>>> Stashed changes
     <Layout/>,
     document.getElementById('root')
 );
