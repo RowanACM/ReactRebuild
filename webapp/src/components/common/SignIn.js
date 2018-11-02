80b29dcf84eab3a632729990c5ccc1bbe14a36cb
Tyler W <jtwalder@gmail.com>
2018-11-02 14:41:00 -0400

    404 page and sign in page stubs

---
+++ webapp/src/components/common/SignIn.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+
+import '../../../App.css';
+
+
+
+
+
+export default class Page404 extends React.Component {
+    constructor(props) {
+        super(props);
+        
+
+    }
+  
+    render() {
+
+
+        return (<div></div>
+        );
+
+
+    }
+}
+
+
