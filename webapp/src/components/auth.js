87bde73c9e3b18869a5e7f8b459a789c708efa69
Mark <markvadeika@gmail.com>
2019-03-08 13:07:02 -0500

    Formatting

--- webapp/src/components/auth.js
+++ webapp/src/components/auth.js
@@ -1,5 +1,6 @@
-import {auth} from './FirebaseKeys';
+import {auth} from 'firebase';
 import firebase from 'firebase';
+
 export const doCreateUserWithEmailAndPassword = (email, password) =>
 auth.createUserWithEmailAndPassword(email, password);
 
@@ -12,8 +13,7 @@ export const doPasswordReset = (email) =>
 auth.sendPasswordResetEmail(email);
 
 // Password Change
-export const doPasswordUpdate = (password) =>
-auth.currentUser.updatePassword(password);
+export const doPasswordUpdate = (password) => auth.currentUser.updatePassword(password);
 
 export function signIn(event,func){
     var provider = new firebase.auth.GoogleAuthProvider();
@@ -28,8 +28,8 @@ export function signIn(event,func){
       var email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       var credential = error.credential;
-      
-      
+
+
     });
-    
+
   }
