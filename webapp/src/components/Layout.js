d72ea0a0755e1ae3bc7462cb127c6a4839d48a9d
Mark <markvadeika@gmail.com>
2019-03-08 13:08:11 -0500

    Formatting

--- webapp/src/components/Layout.js
+++ webapp/src/components/Layout.js
@@ -16,14 +16,14 @@ export default class Layout extends React.Component {
         }
         
         this.signIn=this.signIn.bind(this);
-        
-        
+
+
     }
       signIn=function(event){
         var self=this;
         signIn(event,function(result){
-            
-           
+
+
             fetch("/SignIn", {
                 method: 'POST',
                 /*mode:'no-cors',*/
@@ -32,17 +32,17 @@ export default class Layout extends React.Component {
                     'Content-Type': 'application/json'
                   },
                 body: JSON.stringify({
-                    
+
                     email:result.user.email,
                     pictureUrl:result.additionalUserInfo.profile.picture,
                     firstname: result.additionalUserInfo.profile.given_name,
                     lastname: result.additionalUserInfo.profile.family_name,
                     loginToken: result.credential.accessToken,
                     googleUid: result.user.uid,
-                    
+
                 })
         }).then((res)=>res.json()).then(function(res){
-            
+
             self.setState({isAdmin:res.isAdmin,user:result.user,credentials:result.credential});
         });
     })}
@@ -55,7 +55,7 @@ export default class Layout extends React.Component {
                 <div >
                     <BrowserRouter>
                     <div>
-                        <Navigation signIn={this.signIn} isAdmin={this.state.isAdmin} user={this.state.user}/>
+                        <Navigation /*signIn={this.signIn}*/ isAdmin={this.state.isAdmin} user={this.state.user}/>
                         <hr/>
                         <div>
                             <Routes isAdmin={this.state.isAdmin} user={this.state.user} adminUid={this.state.user&&this.state.user.uid?this.state.user.uid:null} adminToken={this.state.credentials!=null&&this.state.credentials.accessToken?this.state.credentials.accessToken:null}/>
