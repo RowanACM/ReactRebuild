1de63b3127af59b07090ab550f84a06b90c9a87e
Mark <markvadeika@gmail.com>
2019-04-12 23:32:53 -0400

    Removed imports

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './components/Layout';
-import Jumbotron from "./components/common/Jumbotron"
-import CommitteeMemberCard from'./components/pages/committees/committee_members/CommitteeMemberCard';
-import AnnouncementForm from './components/pages/admin/AnnouncementForm';
-import CommitteeMemberCardList from './components/pages/committees/committee_members/CommitteeMemberCardList';
-
-
 
 ReactDOM.render(
     <Layout/>,
