208ea4361c91eb8ecfd68e806e466d4408a7d13a
Mark <markvadeika@gmail.com>
2019-04-12 23:34:46 -0400

    Changed property names

--- webapp/src/components/pages/announcements/AnnouncementCard.js
+++ webapp/src/components/pages/announcements/AnnouncementCard.js
@@ -1,21 +1,19 @@
 import React from 'react'
 
 
-export default class AnnouncmentCard extends React.Component {
+export default class AnnouncementCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: props.title,
-            desc: props.text,
-            postedDate: props.postedDate,
-            picUrl: props.imageUrl,
-            committee: props.committee,
-            externalLink: props.externalLink,
+            title: props.title,
+            text: props.text,
+            date: props.date,
+            author: props.author,
+            link: props.link, // Optional
             color: props.color // Optional
         };
 
         this.colorStyle = this.state.color ? {backgroundColor: this.state.color}: {};
-        console.log(this.props.color);
 
 }
 
@@ -38,20 +36,26 @@ export default class AnnouncmentCard extends React.Component {
         return (
 
             <div class="card" style={style}>
-                {this.state.picUrl ? <img class="car-img-top" src={this.state.picURL} alt="Card image cap"/> : null}
-                <div class="card-header text-white primary" style={this.colorStyle}><h3>{this.state.name}</h3></div>
+                <div class="card-header text-white primary" style={this.colorStyle}><h3>{this.state.title}</h3></div>
                 <div class="card-body">
-                    <p>{this.state.desc}</p>
-                    {this.state.externalLink ? <a href={this.state.externalLink}><button class="btn primary float-right" style={this.colorStyle}>Read more</button></a> : null}
+                    <p>{this.state.text}</p>
+                    {this.state.link ? <a href={this.state.link}><button class="btn primary float-right" style={this.colorStyle}>Read more</button></a> : null}
                 </div>
                 <div class="card-footer text-right">
-                    <small>Posted By {this.state.committee} on {this.props.postedDate}</small>
+                    <small>Posted By {this.state.author} on {this.timeToDate(this.state.date)}</small>
                 </div>
 
             </div>
 
 
         )
+    }
+
+    timeToDate(s) {
+
+        let d = new Date(parseInt(s));
+        return d.toString();
+
     }
 
 }
